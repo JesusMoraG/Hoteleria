e585351b3e8a5310a2f61725a3ed0d65132e4b9b
JesusMoraG <ing.jesusgalan@gmail.com>
2025-09-15 08:57:47 -0600

    Implementación de servicio usuarios

---
+++ Hoteleria/msv-usuarios/src/main/java/com/tcg/usuarios/MsvUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.tcg.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvUsuariosApplication.class, args);
+	}
+
+}
