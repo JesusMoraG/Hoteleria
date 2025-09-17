ea2007f6b48017b1ec3f0443708bd44116b8bf79
JesusMoraG <83291820+JesusMoraG@users.noreply.github.com>
2025-09-17 08:53:11 -0600

    Delete Hoteleria/msv-huespedes directory para corrección de errores

--- Hoteleria/msv-huespedes/client/ReservasClient.java
+++
@@ -1,8 +0,0 @@
-package com.tcg.huespedes.client;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-@FeignClient(name = "msv-reservas", path = "/api/reservas")
-public interface ReservasClient {
-    @GetMapping("/ping")
-    String ping();
-}
